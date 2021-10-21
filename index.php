e57971447616c910e3f9a43bddff402e55b11dd3
J-Tarang0 <91654875+J-Tarang0@users.noreply.github.com>
2021-10-21 12:46:40 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo"Software diseñado por José Alberto Mendoza Tarango";
+echo"Número de control: 0649";
+echo"Estructura selectiva if-else";
+    $x = 80;
+
+    if( $x >= 100 )
+    {
+        echo "Mayor o igual de 100";
+    }
+    else
+    {
+        echo "Menor de 100";
+    }
+?>
